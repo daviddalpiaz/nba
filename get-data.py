f10448c25844b7ba64d8bfc7ec48f6a7a5e0980e
David Dalpiaz <dalpiaz2@illinois.edu>
2024-02-09 10:44:30 -0600

    updates from friday class

--- get-data.py
+++ get-data.py
@@ -10,15 +10,31 @@ def convert_time_format(time_str):
     return time_str.replace("a", "AM").replace("p", "PM")
 
 
+# working towards obtaining all data
+base_url = "http://www.basketball-reference.com/leagues/NBA_2016_games-"
+months = ["October", "November", "December", "January", "February", "March", "April", "May", "June"]
+urls = [base_url + month.lower() + ".html" for month in months]
+
 # path to store raw data
-path_data_raw = "data-raw/nba.csv"
+path_data_raw = "data-raw/nba-rest-days.csv"
 
 if not os.path.exists(path_data_raw):
-    tables = pd.read_html("http://www.basketball-reference.com/leagues/NBA_2016_games.html")
-    games = tables[0]
+    month_data = []
+    for url in urls:
+        tables = pd.read_html(url)
+        games_month = tables[0]
+        month_data.append(games_month)
+    games = pd.concat(month_data)
     games.to_csv(path_data_raw)
 else:
-    games = pd.read_csv(path_data_raw)
+    games = pd.read_csv(path_data_raw, index_col="Unnamed: 0")
+
+# debugging monthly tables
+april = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2016_games-april.html")
+april = april[0]
+april["PTS"].tolist()
+
+# TODO: deal with "playoff" intra-data header that causes column type issues
 
 # current column names from basketball-reference.com mapped to names for df
 column_names = {
@@ -60,8 +76,3 @@ games_tidy_location["rest"] = (
     games_tidy_location.sort_values("date").groupby("team").date.diff().dt.days - 1
 )
 games_tidy_location.dropna()
-
-# working towards obtaining all data
-base_url = "http://www.basketball-reference.com/leagues/NBA_2016_games-"
-months = ["October", "November", "December", "January", "February", "March", "April", "May", "June"]
-urls = [base_url + month.lower() + ".html" for month in months]
