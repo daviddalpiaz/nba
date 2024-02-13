6353a6eb2b0563b97811f23374cd7dbccb8df9c7
David Dalpiaz <dalpiaz2@illinois.edu>
2024-02-12 19:21:25 -0600

    finish data prep

--- get-data.py
+++ get-data.py
@@ -15,6 +15,9 @@ base_url = "http://www.basketball-reference.com/leagues/NBA_2016_games-"
 months = ["October", "November", "December", "January", "February", "March", "April", "May", "June"]
 urls = [base_url + month.lower() + ".html" for month in months]
 
+# get "good" column types
+column_types = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2016_games-december.html")[0].dtypes
+
 # path to store raw data
 path_data_raw = "data-raw/nba-rest-days.csv"
 
@@ -25,20 +28,16 @@ if not os.path.exists(path_data_raw):
         games_month = tables[0]
         month_data.append(games_month)
     games = pd.concat(month_data)
-    games.to_csv(path_data_raw)
+    games = games[games["Date"] != "Playoffs"]
+    games["PTS"] = games["PTS"].astype("int64")
+    games["PTS.1"] = games["PTS.1"].astype("int64")
+    games["Attend."] = games["Attend."].astype("int64")
+    games.to_csv(path_data_raw, index=False)
 else:
-    games = pd.read_csv(path_data_raw, index_col="Unnamed: 0")
-
-# debugging monthly tables
-april = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2016_games-april.html")
-april = april[0]
-april["PTS"].tolist()
-
-# TODO: deal with "playoff" intra-data header that causes column type issues
+    games = pd.read_csv(path_data_raw)
 
 # current column names from basketball-reference.com mapped to names for df
 column_names = {
-    "Unnamed: 0": "game_id",
     "Date": "date",
     "Start (ET)": "start",
     "Visitor/Neutral": "away_team",
@@ -57,7 +56,6 @@ games = games.rename(columns=column_names)
 games = games.assign(date=lambda x: pd.to_datetime(x["date"], format="%a, %b %d, %Y"))
 games["start"] = pd.to_datetime(games["start"].apply(convert_time_format), format="%I:%M%p").dt.time
 games = games.sort_values(["date", "start"], ascending=[True, True])
-games.drop(columns=["game_id"], inplace=True)
 games.insert(2, "date_game_id", games.groupby("date").cumcount())
 games
 
@@ -69,10 +67,11 @@ games_tidy_location = pd.melt(
     value_name="team",
     var_name="location",
 )
-games_tidy_location["location"] = games_tidy_location["location"].replace(
-    {"away_team": "away", "home_team": "home"}
-)
-games_tidy_location["rest"] = (
-    games_tidy_location.sort_values("date").groupby("team").date.diff().dt.days - 1
-)
+games_tidy_location["location"] = games_tidy_location["location"].replace({"away_team": "away", "home_team": "home"})
+games_tidy_location["rest"] = games_tidy_location.sort_values("date").groupby("team").date.diff().dt.days - 1
 games_tidy_location.dropna()
+
+rest_by_team = games_tidy_location.groupby(["team", "location"])["rest"].agg(["mean", "std"])
+rest_by_team = rest_by_team.reset_index()
+rest_by_team
+rest_by_team.to_csv("data/rest.csv", index=False)
